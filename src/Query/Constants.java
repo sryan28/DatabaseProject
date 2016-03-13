30aa17c82a2cd8725046417a00cc5207ea588c31
sryan28 <sean.ryan3@mail.mcgill.ca>
2016-03-12 23:28:49 -0500

    Inital commit

---
+++ src/Query/Constants.java
@@ -0,0 +1,23 @@
+package Query;
+
+/*
+ * Constants for querying an entire table of the database without filtering
+ */
+
+public class Constants {
+	
+	private final static String GETALLSONGS = "SELECT * FROM song";
+	private final static String GETALLARTISTS = "SELECT * FROM artist";
+	private final static String GETALLEPISODES = "SELECT * FROM episode";
+	private final static String GETALLSEASONS = "SELECT * FROM season";
+	private final static String GETALLMOVIES = "SELECT * FROM movie";
+	private final static String GETALLPLAYLISTS = "SELECT * FROM playlist";
+	private final static String GETALLMEDIA = "SELECT * FROM media";
+	private final static String GETALLCUSTOMERS = "SELECT * FROM customer";
+	private final static String GETALLALBUMS = "SELECT *FROM album";
+	private final static String GETALLNETWORKS = "SELECT * FROM network";
+	private final static String GETALLTVSERIES = "SELECT * FROM tvseries";
+	private final static String GETALLSTUDIOS = "SELECT * FROM studio";
+	private final static String GETALLACTORS = "SELECT * FROM actor";
+			
+}
---
+++ src/Query/Queries.java
@@ -0,0 +1,76 @@
+package Query;
+
+import java.sql.*;
+
+/*
+ * This class is for opening and closing the database 
+ * Use queryDatabse method for more querying
+ * 
+ * When using this class make sure you check the return ResultSet is not null!
+ * Remember to close the database after you use it!
+ */
+
+public class Queries {
+
+	private final static String PASSWORD = "Cake=Lie1";
+	private final static String USERNAME = "";
+	
+
+	/*
+	 * Opens the database and returns a connection
+	 */
+	public Connection openDatabase() {
+
+		Connection con = null;
+
+		/*
+		 * Showing error for some reason
+		 */
+		//		try {
+		//			DriverManager.registerDriver(new com.ibm.db2.jcc.DB2Driver());
+		//		} catch(Exception e) {
+		//			e.printStackTrace();
+		//		}
+
+		String url = "jdbc:db2://comp421.cs.mcgill.ca.50000/cs421";
+
+		try {
+			con = DriverManager.getConnection(url, USERNAME, PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+
+	/*
+	 * Closes the database connection
+	 */
+	public void closeDatabase(Connection con) {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * This is for general querying
+	 */
+	public ResultSet queryDatabase(Connection con, String query) {
+		ResultSet results = null;
+		
+		Statement stmt;
+		try {
+			stmt = con.createStatement();
+			results = stmt.executeQuery(query);
+			
+			stmt.close();
+			return results;
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
